abb1396b402e08300ecc085537846c4de39c124c
blssblgts <balagtas.blessie.e@gmail.com>
2023-04-04 14:19:19 +0800

    update

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from 'app/product.service';
+
+
+@Component( {
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrl: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+
+  albumInfo;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
+
+  }
+}
